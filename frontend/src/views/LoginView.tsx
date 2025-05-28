e7ac3467d38aad84945876affaec77e300266537
Jesus Flores <jfloressalazar91@gmail.com>
2025-05-27 21:54:02 -0600

    Clase 49, inclusion de links de navegacion haciendo uso de LInk de react para crear enlaces optimizados

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
+
 export default function LoginView() {
     return (
-        <h1>LoginView</h1>
+        <>
+            <h1>LoginView</h1>
+            <nav>
+
+                <Link to="/auth/register">Register</Link>
+
+            </nav>
+        </>
     );
 }
--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function RegisterView() {
     return (
-        <h1>RegisterView</h1>
+        <>
+            <h1>RegisterView</h1>
+            <nav>
+
+                <Link to="/auth/login">Login</Link>
+
+            </nav>
+        </>
     );
 }
